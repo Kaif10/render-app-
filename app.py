4968807937a149da6b4d6cc9c81f514dc4be460d
Kaif Kohari <kaifkohari10@gmail.com>
2023-05-13 03:52:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,31 @@
-import streamlit as st
-import pickle
 import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
 
-# Load the trained model
+app = Flask(__name__)
 model = pickle.load(open("model.pkl", "rb"))
 
-# Define a function to predict the salary based on the input features
-def predict_salary(experience, test_score, interview_score):
-    # Convert the input to a numpy array
-    input_features = np.array([[experience, test_score, interview_score]])
-    # Use the trained model to make a prediction
-    prediction = model.predict(input_features)[0]
-    return prediction
 
-# Define the Streamlit app
-def main():
-    # Set the title and the header
-    st.title("Salary Predictor")
-    st.header("Enter the following details to predict the salary:")
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-    # Create input fields for the three features
-    experience = st.slider("Experience", 0, 12, 5)
-    test_score = st.slider("Test Score", 0, 100, 50)
-    interview_score = st.slider("Interview Score", 0, 10, 5)
 
-    # Display a button to make the prediction
-    if st.button("Predict Salary"):
-        # Call the predict_salary function to make the prediction
-        prediction = predict_salary(experience, test_score, interview_score)
-        # Display the prediction
-        st.success(f"Predicted Salary is $ {prediction:.2f}")
+@app.route("/predict", methods=["POST"])
+def predict():
+    """
+    For rendering results on HTML GUI
+    """
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template(
+        "index.html", prediction_text="Predicted Salary $ {}".format(output)
+    )
+
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
